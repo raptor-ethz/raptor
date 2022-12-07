85bb3658746650852129eb77c290c4a30e292d1d
Pascal <pstrauch@ethz.ch>
2022-12-07 17:55:46 +0100

    Implement publisher

--- src/vicon_interface/src/vicon_interface.cpp
+++ src/vicon_interface/src/vicon_interface.cpp
@@ -6,30 +6,56 @@
 // custom ros interface
 #include "raptor_interface/msg/pose.hpp"
 
+
+// parameters TODO
+const int PUBLISHER_INTERVAL_MS = 1;
+
+
+class ViconPublisher : public rclcpp::Node
+{
+public:
+  ViconPublisher()
+  : Node("vicon_publisher"), count_(0)
+  {
+    publisher_ =
+      this->create_publisher<raptor_interface::msg::Pose>("pose", 10);
+    
+    timer_ = this->create_wall_timer(
+      std::chrono::milliseconds(PUBLISHER_INTERVAL_MS),
+      std::bind(&ViconPublisher::publisherCallback, this));
+  }
+
+private:
+  void publisherCallback() 
+  {
+    auto message = raptor_interface::msg::Pose();
+    message.x = 1;
+
+    // DEBUG
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Publishing: [%f]", message.x);
+    publisher_->publish(message);
+  }
+
+  rclcpp::Publisher<raptor_interface::msg::Pose>::SharedPtr publisher_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  size_t count_;
+};
+
+
+
 int main(int argc, char *argv[])
 {
   // initialize ros
   rclcpp::init(argc, argv);
 
-  // // check input
-  // if (argc < 2)
-  // {
-  //   std::cerr << "Port not provided.\n";
-  //   usage(argv[0]);
-  //   return 1;
-  // }
+  // create node
+  auto node = std::make_shared<ViconPublisher>();
 
-  // // start service node
-  // auto interface = std::make_shared<Quad>();
-  // if ( !(interface->initialize(argv[1])) ) {
-  //   rclcpp::shutdown();
-  //   return 1;
-  // }
+  // inform
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Vicon interface server is ready.");
 
-  // RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "MavSDK interface server is ready.");
-
-  // // spin node
-  // rclcpp::spin(interface);
-  // rclcpp::shutdown();
+  // spin node
+  rclcpp::spin(node);
+  rclcpp::shutdown();
   return 0;
 }
