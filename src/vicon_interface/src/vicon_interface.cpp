0f77b43c675708c1b4484a5a08dfdc3a15e71f22
Pascal <pstrauch@ethz.ch>
2022-12-07 17:24:24 +0100

    Create pose msg

--- src/vicon_interface/src/vicon_interface.cpp
+++ src/vicon_interface/src/vicon_interface.cpp
@@ -2,34 +2,34 @@
 #include <iostream>
 #include <chrono>
 
-#include "rclcpp/rclcpp.hpp"       // ros2 client library
-
-// #include "geometry_msgs/msg/point.hpp"
+#include "rclcpp/rclcpp.hpp" // ros2 client library
+// custom ros interface
+#include "raptor_interface/msg/pose.hpp"
 
 int main(int argc, char *argv[])
 {
   // initialize ros
   rclcpp::init(argc, argv);
 
-  // check input
-  if (argc < 2)
-  {
-    std::cerr << "Port not provided.\n";
-    usage(argv[0]);
-    return 1;
-  }
+  // // check input
+  // if (argc < 2)
+  // {
+  //   std::cerr << "Port not provided.\n";
+  //   usage(argv[0]);
+  //   return 1;
+  // }
 
-  // start service node
-  auto interface = std::make_shared<Quad>();
-  if ( !(interface->initialize(argv[1])) ) {
-    rclcpp::shutdown();
-    return 1;
-  }
+  // // start service node
+  // auto interface = std::make_shared<Quad>();
+  // if ( !(interface->initialize(argv[1])) ) {
+  //   rclcpp::shutdown();
+  //   return 1;
+  // }
 
-  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "MavSDK interface server is ready.");
+  // RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "MavSDK interface server is ready.");
 
-  // spin node
-  rclcpp::spin(interface);
-  rclcpp::shutdown();
+  // // spin node
+  // rclcpp::spin(interface);
+  // rclcpp::shutdown();
   return 0;
 }
