c2471b01e8b301c828d0bc2952d0b05d872ed4da
Pascal <pstrauch@ethz.ch>
2022-12-07 17:06:51 +0100

    Move vicon interface package

---
+++ src/vicon_interface/src/vicon_interface.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <iostream>
+#include <chrono>
+
+#include "rclcpp/rclcpp.hpp"       // ros2 client library
+
+// #include "geometry_msgs/msg/point.hpp"
+
+int main(int argc, char *argv[])
+{
+  // initialize ros
+  rclcpp::init(argc, argv);
+
+  // check input
+  if (argc < 2)
+  {
+    std::cerr << "Port not provided.\n";
+    usage(argv[0]);
+    return 1;
+  }
+
+  // start service node
+  auto interface = std::make_shared<Quad>();
+  if ( !(interface->initialize(argv[1])) ) {
+    rclcpp::shutdown();
+    return 1;
+  }
+
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "MavSDK interface server is ready.");
+
+  // spin node
+  rclcpp::spin(interface);
+  rclcpp::shutdown();
+  return 0;
+}
