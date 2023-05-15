077efa9c9a134a8bd327327b9cd5bfed308c0332
Pascal <pstrauch@ethz.ch>
2023-05-15 19:37:50 +0200

    Add mission control header file skeleton

---
+++ src/mission_control/include/mission_control/mission_control.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <string>
+
+// ros default
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+
+// interfaces
+#include "raptor_interface/srv/trigger.hpp"
+#include "raptor_interface/action/takeoff.hpp"
+#include "raptor_interface/action/go_to_pos.hpp"
+
+
+
+
+class MissionControl : public rclcpp::Node
+{
+public:
+  using Takeoff = raptor_interface::action::Takeoff;
+  using TakeoffGoalHandle = rclcpp_action::ClientGoalHandle<Takeoff>;
+  using GoToPos = raptor_interface::action::GoToPos;
+  using GoToPosGoalHandle = rclcpp_action::ClientGoalHandle<GoToPos>;
+
+  MissionControl();
+  ~MissionControl() {};
+
+
+private:
+  // TODO add quad state
+  
+  rclcpp_action::Client<Takeoff>::SharedPtr act_takeoff_;
+  rclcpp_action::Client<GoToPos>::SharedPtr act_goToPos_;
+
+};
